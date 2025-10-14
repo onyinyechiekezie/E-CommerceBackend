8485d6ba49fd68f48eb404e04ed26f9d4e36d997
0nyinye <onyinyeekezi2017@gmail.com>
2025-10-14 01:51:09 +0100

    UserDomainService

--- src/main/java/com/ecommerce/ecommercebackend/domain/service/UserDomainService.java
+++ src/main/java/com/ecommerce/ecommercebackend/domain/service/UserDomainService.java
@@ -1,4 +1,39 @@
 package com.ecommerce.ecommercebackend.domain.service;
 
+import com.ecommerce.ecommercebackend.domain.model.User;
+import com.ecommerce.ecommercebackend.domain.enums.Role;
+import java.util.UUID;
+
 public class UserDomainService {
+
+    public User createUser(String name, String email, String password, Role role) {
+        return new User(UUID.randomUUID(), name, email, password, role, true,
+                java.time.LocalDateTime.now(), java.time.LocalDateTime.now());
+    }
+
+    public User activateUser(User user) {
+        return new User(
+                user.getId(),
+                user.getName(),
+                user.getEmail(),
+                user.getPassword(),
+                user.getRole(),
+                true,
+                user.getCreatedAt(),
+                java.time.LocalDateTime.now()
+        );
+    }
+
+    public User deactivateUser(User user) {
+        return new User(
+                user.getId(),
+                user.getName(),
+                user.getEmail(),
+                user.getPassword(),
+                user.getRole(),
+                false,
+                user.getCreatedAt(),
+                java.time.LocalDateTime.now()
+        );
+    }
 }
